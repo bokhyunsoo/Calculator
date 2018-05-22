7c1921ce61cc8a2a9a3b095efac7a727a00b1f9c
bok-PC\bok <bok_0120@naver.com>
2018-05-22 19:24:24 +0900

    메소드를 분리해 계산기 기능 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 
 public class Calculator {
-	public static void main(String[] args){
 		
-		Scanner scanner = new Scanner(System.in);
-			
+		static int getFirstValue(Scanner scanner) {
 			System.out.println("입력 값: ");
 			int first = scanner.nextInt();
-			
-			while(true) {
-			
+			return first;
+		}
+		
+		static String getSymbol(Scanner scanner) {
 			System.out.println("기호: ");
 			String symbol = scanner.next();
-			
-			if ("quit".equals(symbol)){
-				System.out.println("최종 값 : "+first);
-				break;
-			}
-			
+			return symbol;
+		}
+		
+		static int getSecondValue(Scanner scanner) {
 			System.out.println("입력 값: ");
 			int second = scanner.nextInt();
-			
-			
+			return second;
+		}
+		
+		static int calculate(String symbol, int first, int second){
 			if(symbol.equals("+")){
 				first = first + second;
 				System.out.println("현재 값 : "+first);
@@ -37,6 +36,33 @@ public class Calculator {
 			} else {
 				System.out.println("사칙연산 값을 잘못 입력하셨습니다.");
 			}
+			
+			return first;
+			
 		}
-	}
+		
+		static void print(int first){
+				System.out.println("최종 값 : "+first);
+		}
+		
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			int first = getFirstValue(scanner);
+			
+			while (true) {
+				String symbol = getSymbol(scanner);
+				
+				if ("quit".equals(symbol)){
+					print(first);
+					break;
+				}
+				
+				int second = getSecondValue(scanner);
+				
+				first = calculate(symbol, first, second);
+			}
+			
+			scanner.close();
+		}
 }

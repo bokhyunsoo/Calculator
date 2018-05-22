7b3f77b84475a2a703af0a68d99922758fe13d0b
bok-PC\bok <bok_0120@naver.com>
2018-05-22 18:45:00 +0900

    계산기 기본 기능 구현 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,25 +4,39 @@ public class Calculator {
 	public static void main(String[] args){
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("첫 번째 입력 값: ");
-		int first = scanner.nextInt();
-		
-		System.out.println("두번째 입력 값: ");
-		int second = scanner.nextInt();
-		
-		System.out.println("사칙연산 값: ");
-		String symbol = scanner.next();
-		
-		if(symbol.equals("+")){
-			System.out.println("덧셈 : "+ (first+second));
-		} else if(symbol.equals("-")){
-			System.out.println("뺄셈 : "+ (first-second));
-		} else if(symbol.equals("*")){
-			System.out.println("곱셈 : "+ (first*second));
-		} else if(symbol.equals("/")){
-			System.out.println("나눗셈 : "+ (first/second));		
-		} else {
-			System.out.println("사칙연산 값을 잘못 입력하셨습니다.");
+			
+			System.out.println("입력 값: ");
+			int first = scanner.nextInt();
+			
+			while(true) {
+			
+			System.out.println("기호: ");
+			String symbol = scanner.next();
+			
+			if ("quit".equals(symbol)){
+				System.out.println("최종 값 : "+first);
+				break;
+			}
+			
+			System.out.println("입력 값: ");
+			int second = scanner.nextInt();
+			
+			
+			if(symbol.equals("+")){
+				first = first + second;
+				System.out.println("현재 값 : "+first);
+			} else if(symbol.equals("-")){
+				first = first - second;
+				System.out.println("현재 값 : "+first);
+			} else if(symbol.equals("*")){
+				first = first * second;
+				System.out.println("현재 값 : "+first);
+			} else if(symbol.equals("/")){
+				first = first / second;
+				System.out.println("현재 값 : "+first);
+			} else {
+				System.out.println("사칙연산 값을 잘못 입력하셨습니다.");
+			}
 		}
 	}
 }
